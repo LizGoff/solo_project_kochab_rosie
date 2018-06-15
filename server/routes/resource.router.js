6d305ae9a06e42e8576c82e99c5490c36810fd64
Liz Goff <livingearthcreations@gmail.com>
2018-06-15 14:08:25 -0500

    resource link page working with new sql join

--- server/routes/resource.router.js
+++ server/routes/resource.router.js
@@ -5,50 +5,38 @@ const router = express.Router();
 // POST
 
 router.post('/', (req, res) => {
-    if (req.isAuthenticated()) {
-      const queryText = `INSERT INTO links ("url", "topic_id", "user_id")
+  if (req.isAuthenticated()) {
+    const queryText = `INSERT INTO links ("url", "topic_id", "user_id")
                       VALUES ($1, $2, $3) RETURNING "url";`;
-      pool.query(queryText, [req.body.url, req.body.topic, req.user.id])
-        .then(() => { res.sendStatus(201); })
-        .catch((err) => {
-          console.log('Error completing POST link query', err);
-          res.sendStatus(500);
-        })
-    } else {
-      res.sendStatus(403);
-    }
-  });
+    pool.query(queryText, [req.body.url, req.body.topic, req.user.id])
+      .then(() => { res.sendStatus(201); })
+      .catch((err) => {
+        console.log('Error completing POST link query', err);
+        res.sendStatus(500);
+      })
+  } else {
+    res.sendStatus(403);
+  }
+});
 
 // GET
 
 router.get('/', (req, res) => {
-    if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
 
-    const queryText = 'SELECT * FROM links';
+    const queryText = `SELECT * FROM "links" 
+    LEFT JOIN "topics" ON "links"."topic_id" = "topics"."id"
+    ORDER BY "links"."topic_id"`;
     pool.query(queryText)
       .then((result) => { res.send(result.rows); })
       .catch((err) => {
         console.log('Error completing GET links query', err);
         res.sendStatus(500);
       });
-    } else {
-      res.sendStatus(403);
-    }
-  
-  });
-  
-  router.get('/:id', (req, res) => {
-    if (req.isAuthenticated()) {
-    const queryText = 'SELECT * FROM links WHERE id=$1';
-    pool.query(queryText, [req.params.id])
-      .then((result) => { res.send(result.rows); })
-      .catch((err) => {
-        console.log('Error completing GET links query', err);
-        res.sendStatus(500);
-      });
-    } else {
-      res.sendStatus(403);
-    }
-  });
+  } else {
+    res.sendStatus(403);
+  }
 
-  module.exports = router;
+});
+
+module.exports = router;
--- src/components/Resource/Resource.js
+++ src/components/Resource/Resource.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+
 
 import Nav from '../../components/Nav/Nav';
 
@@ -13,27 +15,39 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-const mapReduxStateToProps = (reduxState) => (
-  { reduxState }
-);
+const mapStateToProps = state => ({
+  user: state.user,
+});
 
 class ResourcePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      resourceHelp: []
+      resourceHelp: [],
+
     }
   }
 
   componentDidMount() {
+    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+
     this.fetchResourceData();
+    this.setState({
+      topic: this.props.match.params.id
+    })
+  }
+
+  componentDidUpdate() {
+    if (!this.props.user.isLoading && this.props.user.userName === null) {
+      this.props.history.push('home');
+    }
   }
 
   fetchResourceData() {
-    axios.get('/api/resource').then((response) => {
+    axios.get(`/api/resource`).then((response) => {
       console.log(response.data[0]);
       this.setState({
-        resourceHelp: response.data
+        resourceHelp: response.data,
       })
     }).catch((error) => {
       alert('error with GET in addResource file');
@@ -61,9 +75,12 @@ class ResourcePage extends Component {
   render() {
     let content = null
 
-    if (this.props) {
+    if (this.props.user.userName) {
       content = (
         <div>
+          <h1 id="welcome">
+            Thank you for joining the conversation, {this.props.user.userName}!
+          </h1>
           <div>
             <h2>
               Resources
@@ -91,22 +108,20 @@ class ResourcePage extends Component {
                     <TableCell>Resource Link</TableCell>
                   </TableRow>
                 </TableHead>
-
+ 
                 <TableBody>
                   {this.state.resourceHelp.map((linkToUrl, i) => (
                     <TableRow key={i}>
-                      <TableCell>{linkToUrl.url}</TableCell>
+                      <TableCell><strong>{linkToUrl.name}</strong>; {linkToUrl.url}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
-
               </Table>
             </Paper>
           </div >
         </div>
       );
     }
-
     return (
       <div>
         <Nav />
@@ -116,4 +131,4 @@ class ResourcePage extends Component {
   }
 }
 
-export default connect(mapReduxStateToProps)(ResourcePage);
+export default connect(mapStateToProps)(ResourcePage);
