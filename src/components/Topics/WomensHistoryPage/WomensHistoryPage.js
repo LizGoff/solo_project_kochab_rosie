4cf3d094f4aa3590af0edd50acc6665c26a22498
Liz Goff <livingearthcreations@gmail.com>
2018-06-16 15:54:12 -0500

    gallery up and running

--- src/components/Topics/WomensHistoryPage/WomensHistoryPage.js
+++ src/components/Topics/WomensHistoryPage/WomensHistoryPage.js
@@ -1,24 +1,77 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import axios from 'axios';
 import Nav from '../../Nav/Nav';
-// import { USER_ACTIONS } from '../../redux/actions/userActions';
+import { USER_ACTIONS } from '../../../redux/actions/userActions';
+import GalleryList from '../GalleryList/GalleryList';
+
 
 const mapStateToProps = state => ({
-    user: state.user,
-  });
-  
-  class WomensHistoryPage extends Component {
-    render() {
-      let content = null;
-      return (
+  user: state.user,
+});
+
+class WomensHistoryPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      galleryItems: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getFromGalleryItems();
+  }
+
+  getFromGalleryItems = () => {
+
+    axios.get('/gallery')
+      .then((response) => {
+        this.setState({
+          galleryItems: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log('error with GET', error);
+      })
+  }
+
+  addLike = (item) => {
+    console.log('adding likes', item);
+    axios.put(`/gallery/like/${item.id}`)
+
+      .then((response) => {
+        console.log('put response', response);
+        this.getFromGalleryItems();
+      })
+      .catch((error) => {
+        console.log('put/add error in addLike', error);
+      });
+  }
+
+  render() {
+    let content = null
+    if (this.props.user.userName) {
+      content = (
+        <div>
+          {this.props.data}
           <div>
-          <p>Women in our history</p>
-          <Nav />
-          { content }
+            <h1 id="welcome">Thank you for joining the conversation, {this.props.user.userName}!</h1>
+            <h2>Women in our history</h2>
+            <div>
+              <GalleryList galleryItems={this.state.galleryItems}
+                addLike={this.addLike} />
+            </div>
+          </div>
         </div>
       );
     }
+    return (
+      <div>
+        <Nav />
+        {content}
+      </div>
+    );
   }
+}
 
-export default connect(mapStateToProps)(WomensHistoryPage);
+    export default connect(mapStateToProps)(WomensHistoryPage);
