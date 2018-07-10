1ded56aaa33e4e934c012cf27916d18df9672700
Liz Goff <livingearthcreations@gmail.com>
2018-07-10 08:33:14 -0500

    changed local host

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/gallery', gallery);
 
 app.use(express.static('build'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
